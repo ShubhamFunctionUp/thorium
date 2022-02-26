bc4d97ec7d5938d79ab38de7afa3450f491a9093
ShubhamFunctionUp <Shubham9638464483@gmail.com>
2022-02-26 22:22:48 +0530

    book list is created

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,28 @@
-const UserModel= require("../models/userModel")
+const UserModel = require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+// const createUser= async function (req, res) {
+//     let data= req.body
+//     let savedData= await UserModel.create(data)
+//     res.send({msg: savedData})
+// }
+
+// const getUsersData= async function (req, res) {
+//     let allUsers= await UserModel.find()
+//     res.send({msg: allUsers})
+// }
+
+// module.exports.createUser= createUser
+// module.exports.getUsersData= getUsersData
+
+const getBook = async function (req,res) {
+    let allbook = await UserModel.find();
+    res.send({msg:allbook});
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const createBook = async function (req, res) {
+    let data = req.body;
+    let savedBook =await UserModel.create(data);
+    res.send({ msg: savedBook });
 }
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createBook = createBook;
+module.exports.getBook =getBook;
