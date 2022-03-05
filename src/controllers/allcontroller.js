836f37210ee02ffc75cf9071acf1004c13680bc2
ShubhamFunctionUp <shubhamcodingninjas>
2022-03-05 20:56:11 +0530

    Full Completed

--- src/controllers/allcontroller.js
+++ src/controllers/allcontroller.js
@@ -1,30 +1,51 @@
-const bookModel  = require('../models/bookModel');
-const authorModel  = require('../models/authorModel');
-const publisherModel  = require('../models/publisherModel');
+const bookModel = require('../models/bookModel');
+const authorModel = require('../models/authorModel');
+const publisherModel = require('../models/publisherModel');
+const { updateOne, find } = require('../models/bookModel');
 
-const createAuthor = async function(req,res){
+const createAuthor = async function (req, res) {
     let authorDetails = req.body;
-    let savedData =await authorModel.create(authorDetails);
-    res.send({msg:savedData})
+    let savedData = await authorModel.create(authorDetails);
+    res.send({ msg: savedData })
 }
 
-const createPublisher = async function(req,res){
+const createPublisher = async function (req, res) {
     let publisherDetails = req.body;
-    let savedData =await publisherModel.create(publisherDetails)
-    res.send({msg:savedData})
+    let savedData = await publisherModel.create(publisherDetails)
+    res.send({ msg: savedData })
 }
 
-const createBook = async function(req,res){
+const createBook = async function (req, res) {
     let bookDetails = req.body;
     let savedData = await bookModel.create(bookDetails);
-    res.send({msg:savedData})
+    res.send({ msg: savedData })
 }
 
-const allBooks = async function(req,res){
-    let pen =await bookModel.find({publisher}).populate('author publisher');
-    res.send({msg:pen})
-}
-module.exports.allBooks=allBooks
+const allBooks = async function (req, res) {
+    // 5A
+    let hardCoverPublihers = await publisherModel.find({
+        name: { $in: ["Penguin", "HarperCollins"] }
+    })
+    let publisherID = hardCoverPublihers.map((p) => (p._id));
+    await bookModel.updateMany(
+        { publisherID: { $in: publisherID } },
+        { isHardCover: true }
+    );
+
+    let highRatedAuthors = await authorModel.find({ rating: { $gt: 3.5 } });
+    let authorIds = highRatedAuthors.map((a) => a._id);
+
+    await bookModel.updateMany(
+        { author: { $in: authorIds } },
+        { $inc: { price: 10 } }
+    );
+
+    let updatedBooks = await bookModel.find();
+    res.send({ updatedBookCollection: updatedBooks });
+};
+
+
+module.exports.allBooks = allBooks
 module.exports.createBook = createBook;
 module.exports.createPublisher = createPublisher;
 module.exports.createAuthor = createAuthor;
