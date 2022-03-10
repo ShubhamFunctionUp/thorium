6b997d3213354154440dc856cb28de3aea33acec
ShubhamFunctionUp <shubhamcodingninjas@gmail.com>
2022-03-10 08:44:22 +0530

    done

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.m
 app.use('/', route)
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 4000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
