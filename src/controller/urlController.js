c7518f7370b879d1d00333d0fd55cb6ff22a124e
ShubhamFunctionUp <shubham9638464483@gmail.com>
2022-04-06 22:51:16 +0530

    Redis is used

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,7 +1,10 @@
 const urlModel = require('../model/urlModel');
 const shortid = require('shortid');
 const generate = require('meaningful-string');
+const {promisify} = require("util");
+let redis = require('redis')
 // const { options } = require('../route/route');
+
 function validateUrl(value) {
     return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/.test(value);
 }
@@ -13,10 +16,36 @@ let options = {
 }
 
 
+
+//Connect to redis
+const redisClient = redis.createClient(
+    12672,
+  "redis-12672.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+
+redisClient.auth("BNxF5AmAM2GQBhfZzNgqsQ8x6C5lwywn", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 const createUrlShorter = async function (req, res) {
 
-
-
+     const urlCode = shortid.generate().toLowerCase();
+    console.log(urlCode);
     try {
         if (Object.keys(req.body).length == 0) {
             return res.status(400).send({
@@ -30,14 +59,14 @@ const createUrlShorter = async function (req, res) {
         } = req.body;
 
         // console.log(validateUrl(longUrl));
-        console.log('Random String: ', generate.random(options));
+        // console.log('Random String: ', generate.random(options));
         if (validateUrl(longUrl)) {
 
-            // let url = await urlModel.findOne({longUrl:longUrl})
-            //    if(url){
-            //        return res.status(200).send({status:false,message:"URl is already exists"})
-            //    }else{
-            // const urlCode = shortid.generate();
+            let url = await urlModel.findOne({longUrl:longUrl}).select({"createdAt":0,"updatedAt":0,"__v":0,"_id":0})
+               if(url){
+                   return res.status(409).send({status:false,msg:"Already Present",data:url})
+               }else{
+            // const urlCode = shortid.generate().toLowerCase();
 
             const urlCode = generate.random(options);
             // console.log(shortid);   
@@ -85,7 +114,7 @@ const createUrlShorter = async function (req, res) {
                     });
                 }
             }
-            //    }
+               }
         } else {
             return res.status(400).send({
                 status: false,
@@ -113,23 +142,34 @@ const getUrlCode = async function (req, res) {
 
     try {
 
-        let urlCodeIsPresent = await urlModel.findOne({
-            urlCode: urlCode
-        })
-        // null is falsy value and in if condition it takes only true value so 
-        // when nothing is present it will give null and that is falsy so ! is reverse
-        // logic part to true move inside the code
+        // let urlCodeIsPresent = await urlModel.findOne({
+        //     urlCode: urlCode
+        // })
+        // // null is falsy value and in if condition it takes only true value so 
+        // // when nothing is present it will give null and that is falsy so ! is reverse
+        // // logic part to true move inside the code
 
 
-        if (!urlCodeIsPresent) {
-            return res.status(404).send({
-                status: false,
-                msg: "Url is not present"
-            });
-        }
-        console.log(urlCodeIsPresent.longUrl, urlCodeIsPresent.shortUrl);
-        return res.status(301).redirect(urlCodeIsPresent.longUrl)
+        // if (!urlCodeIsPresent) {
+        //     return res.status(404).send({
+        //         status: false,
+        //         msg: "Url is not present"
+        //     });
+        // }
+        // console.log(urlCodeIsPresent.longUrl, urlCodeIsPresent.shortUrl);
+        // return res.status(301).redirect(urlCodeIsPresent.longUrl)
         // return res.send({status:true,data:urlCodeIsPresent.longUrl})
+        let cachedUrl = await GET_ASYNC(`${urlCode}`);
+        if(cachedUrl){
+            return res.send({data:JSON.parse(cachedUrl)});
+        }else{
+            let getUrl = await urlModel.findOne({urlCode:urlCode});
+            await SET_ASYNC(`${urlCode}`,JSON.stringify(getUrl))
+            res.send({data:getUrl})
+        }
+
+
+
     } catch (err) {
         return res.status(500).send({
             message: err.message
