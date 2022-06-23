a1dfcd0afeeca7f90bdebf7c5584cf9a20270a55
ShubhamFunctionUp <shubham9638464483@gmail.com>
2022-06-23 21:50:21 +0530

    Done

---
+++ commerce/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import  Home  from './component/Home';
+import Navbar from './component/Navbar';
+import {Switch,Route} from 'react-router-dom'
+import Products from './component/Products';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/products" component={Products}/>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
